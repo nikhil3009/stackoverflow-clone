4d5d162767a606166c583251f36a9bd43e834e9a
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2024-01-26 19:34:30 -0500

    Answer question component-created

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,48 +2,109 @@
 
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { BADGE_CRITERIA } from '@/constants';
+import { BadgeCounts } from '@/types';
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
-export const getTimestamp = (createdAt: Date): string => {
-	const now = new Date().getTime(); // Get current time in milliseconds
-	const past = new Date(createdAt).getTime(); // Convert input date to milliseconds
-	const diffInSeconds = Math.floor((now - past) / 1000);
-	const diffInMinutes = Math.floor(diffInSeconds / 60);
-	const diffInHours = Math.floor(diffInMinutes / 60);
-	const diffInDays = Math.floor(diffInHours / 24);
-	const diffInWeeks = Math.floor(diffInDays / 7);
-	const diffInMonths = Math.floor(diffInDays / 30);
-	const diffInYears = Math.floor(diffInDays / 365);
 
-	if (diffInSeconds < 60) {
-		return `${diffInSeconds} seconds ago`;
-	} else if (diffInMinutes < 60) {
-		return `${diffInMinutes} minutes ago`;
-	} else if (diffInHours < 24) {
-		return `${diffInHours} hours ago`;
-	} else if (diffInDays < 7) {
-		return `${diffInDays} days ago`;
-	} else if (diffInWeeks < 4) {
-		return `${diffInWeeks} weeks ago`;
-	} else if (diffInMonths < 12) {
-		return `${diffInMonths} months ago`;
+export const getTimestamp = (createdAt: Date): string => {
+	const now = new Date();
+	const timeDifference = now.getTime() - createdAt.getTime();
+
+	// Define time intervals in milliseconds
+	const minute = 60 * 1000;
+	const hour = 60 * minute;
+	const day = 24 * hour;
+	const week = 7 * day;
+	const month = 30 * day;
+	const year = 365 * day;
+
+	if (timeDifference < minute) {
+		const seconds = Math.floor(timeDifference / 1000);
+		return `${seconds} ${seconds === 1 ? 'second' : 'seconds'} ago`;
+	} else if (timeDifference < hour) {
+		const minutes = Math.floor(timeDifference / minute);
+		return `${minutes} ${minutes === 1 ? 'minute' : 'minutes'} ago`;
+	} else if (timeDifference < day) {
+		const hours = Math.floor(timeDifference / hour);
+		return `${hours} ${hours === 1 ? 'hour' : 'hours'} ago`;
+	} else if (timeDifference < week) {
+		const days = Math.floor(timeDifference / day);
+		return `${days} ${days === 1 ? 'day' : 'days'} ago`;
+	} else if (timeDifference < month) {
+		const weeks = Math.floor(timeDifference / week);
+		return `${weeks} ${weeks === 1 ? 'week' : 'weeks'} ago`;
+	} else if (timeDifference < year) {
+		const months = Math.floor(timeDifference / month);
+		return `${months} ${months === 1 ? 'month' : 'months'} ago`;
 	} else {
-		return `${diffInYears} years ago`;
+		const years = Math.floor(timeDifference / year);
+		return `${years} ${years === 1 ? 'year' : 'years'} ago`;
 	}
 };
-export const formatNumberWithSuffix = (number: number): string => {
-	// Divide and format for millions
-	if (number >= 1_000_000) {
-		return (number / 1_000_000).toFixed(2) + 'M';
+
+export const formatAndDivideNumber = (num: number): string => {
+	if (num >= 1000000) {
+		const formattedNum = (num / 1000000).toFixed(1);
+		return `${formattedNum}M`;
+	} else if (num >= 1000) {
+		const formattedNum = (num / 1000).toFixed(1);
+		return `${formattedNum}K`;
+	} else {
+		return num.toString();
 	}
-	// Divide and format for thousands
-	else if (number >= 1_000) {
-		return (number / 1_000).toFixed(2) + 'K';
-	}
-	// Return the number as is if less than 1000
-	else {
-		return number.toString();
-	}
+};
+
+export const getJoinedDate = (date: Date): string => {
+	// Extract the month and year from the Date object
+	const month = date.toLocaleString('default', { month: 'long' });
+	const year = date.getFullYear();
+
+	// Create the joined date string (e.g., "September 2023")
+	const joinedDate = `${month} ${year}`;
+
+	return joinedDate;
+};
+
+interface UrlQueryParams {
+	params: string;
+	key: string;
+	value: string | null;
+}
+
+interface RemoveUrlQueryParams {
+	params: string;
+	keysToRemove: string[];
+}
+
+interface BadgeParam {
+	criteria: {
+		type: keyof typeof BADGE_CRITERIA;
+		count: number;
+	}[];
+}
+
+export const assignBadges = (params: BadgeParam) => {
+	const badgeCounts: BadgeCounts = {
+		GOLD: 0,
+		SILVER: 0,
+		BRONZE: 0,
+	};
+
+	const { criteria } = params;
+
+	criteria.forEach((item) => {
+		const { type, count } = item;
+		const badgeLevels: any = BADGE_CRITERIA[type];
+
+		Object.keys(badgeLevels).forEach((level: any) => {
+			if (count >= badgeLevels[level]) {
+				badgeCounts[level as keyof BadgeCounts] += 1;
+			}
+		});
+	});
+
+	return badgeCounts;
 };
