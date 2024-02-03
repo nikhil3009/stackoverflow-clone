07eeedd10b7c610a2a24adb74788cbb16f1e42c7
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2024-02-03 17:18:31 -0500

    f

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,8 +3,8 @@
  *
  * @format
  */
+/* tslint:disable:variable-name */
 
-/* eslint-disable camelcase */
 import { Webhook } from 'svix';
 import { headers } from 'next/headers';
 import { WebhookEvent } from '@clerk/nextjs/server';
